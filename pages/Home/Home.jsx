437e2a609cbfe87fd7927a05398f07399c379ba3
Thomas Gouvrillon <thomasgouvrillon@MacBook-Air-de-Thomas.local>
2024-01-24 22:25:05 +0100

    feat : fix build

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -8,9 +8,37 @@ import SectionSkills from "../../components/SectionSkills/SectionSkills.jsx";
 import SectionContact from "../../components/SectionContact/SectionContact.jsx";
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
-import { HOME_CONST_PROJECTS } from "./HomeConst.js";
 import isEqual from "lodash/isEqual";
 import classnames from "classnames";
+
+const HOME_CONST_PROJECT = {
+  image: "/images/screenshots/AMAZON.png",
+  title: "E-shop pour site",
+  description:
+    "Implémentation e-commerce sur site existant. Gestion dynamique du catalogue depuis un backoffice vers Algolia.",
+  technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
+  tags: ["front", "back", "design", "architecture"],
+  href: "/project",
+};
+
+const HOME_CONST_PROJECT_2 = {
+  image: "/images/screenshots/APTIPHAR.png",
+  title: "E-shop pour site",
+  description:
+    "Implémentation e-commerce sur site existant. Gestion dynamique du catalogue depuis un backoffice vers Algolia.",
+  technos: ["HTML", "CSS", "JAVASCRIPT", "REACT"],
+  tags: ["front", "back", "design", "architecture"],
+  href: "/project",
+};
+
+const HOME_CONST_PROJECTS = [
+  HOME_CONST_PROJECT,
+  HOME_CONST_PROJECT_2,
+  HOME_CONST_PROJECT,
+  HOME_CONST_PROJECT_2,
+  HOME_CONST_PROJECT,
+  HOME_CONST_PROJECT_2,
+];
 
 const Home = () => {
   const [darkMode, setDarkMode] = useState(false);
