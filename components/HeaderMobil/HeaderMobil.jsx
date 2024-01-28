128eafaddff9fea0cdfb87b3d7b450d7203d80f7
Thomas Gouvrillon <thomasgouvrillon@MacBook-Air-de-Thomas.local>
2024-01-29 00:28:25 +0100

    feat : wip header mobil

--- components/HeaderMobil/HeaderMobil.jsx
+++ components/HeaderMobil/HeaderMobil.jsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import classnames from "classnames";
 import "./HeaderMobil.css";
 import Icon from "../Icon/Icon.jsx";
-import { func, bool } from "prop-types";
+import Toogle from "../Toogle/Toogle.jsx";
+import Nav from "../Nav/Nav.jsx";
+import { func, bool, object } from "prop-types";
+import get from "lodash/get";
 
-const HeaderMobil = ({ darkMode, setDarkMode }) => {
+const HeaderMobil = ({ darkMode, setDarkMode, navConst }) => {
   const [displayMenu, setDisplayMenu] = useState(false);
   return (
     <div
@@ -13,19 +16,80 @@ const HeaderMobil = ({ darkMode, setDarkMode }) => {
         "HeaderMobil--darkMode": darkMode,
       })}
     >
-      <div onClick={() => setDisplayMenu(!displayMenu)}>
+      <div
+        onClick={() => setDisplayMenu(!displayMenu)}
+        className="HeaderMobil__hamburger"
+      >
         {displayMenu ? <Icon icon="CROSS" /> : <Icon icon="HAMBURGER" />}
       </div>
-      <input
-        type="checkbox"
-        checked={darkMode}
+      <div
+        className={classnames("HeaderMobil__navs", {
+          "HeaderMobil__navs--display": displayMenu,
+        })}
+      >
+        <Nav
+          label={get(navConst, "home.label", "")}
+          href={get(navConst, "home.href", "")}
+          darkMode={darkMode}
+        />
+        <Nav
+          label={get(navConst, "profile.label", "")}
+          href={get(navConst, "profile.href", "")}
+          darkMode={darkMode}
+        />
+        <Nav
+          label={get(navConst, "projects.label", "")}
+          href={get(navConst, "projects.href", "")}
+          darkMode={darkMode}
+        />
+        <Nav
+          label={get(navConst, "contact.label", "")}
+          href={get(navConst, "contact.href", "")}
+          darkMode={darkMode}
+        />
+      </div>
+      <div
+        className={classnames("HeaderMobil__icons", {
+          "HeaderMobil__icons--display": displayMenu,
+        })}
+      >
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={get(navConst, "linkedin.href", "")}
+          className={classnames("HeaderMobil__icon", {
+            "HeaderMobil__icon--darkMode": darkMode,
+          })}
+        >
+          <Icon icon={"LINKEDIN"} />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={get(navConst, "github.href", "")}
+          className={classnames("HeaderMobil__icon", {
+            "HeaderMobil__icon--darkMode": darkMode,
+          })}
+        >
+          <Icon icon={"GITHUB"} />
+        </a>
+      </div>
+      <Toogle
+        iconTrue="MOON"
+        iconFalse="SUN"
         onChange={() => setDarkMode(!darkMode)}
+        value={darkMode}
+        darkMode={darkMode}
+        className={classnames("HeaderMobil__toogle", {
+          "HeaderMobil__toogle--display": displayMenu,
+        })}
       />
     </div>
   );
 };
 
 HeaderMobil.propTypes = {
+  navConst: object.isRequired,
   setDarkMode: func.isRequired,
   darkMode: bool,
 };
