2d344f247ac534055437032c29930901d0f92fa5
Thomas Gouvrillon <thomasgouvrillon@MacBook-Air-de-Thomas.local>
2024-02-06 13:59:02 +0100

    feat : finalize wording

--- const/projects.js
+++ const/projects.js
@@ -11,98 +11,14 @@ const projects = [
     description:
       "Conception, réalisation et maintenance front d'un back office de gestion de messagerie, d'e-commerce et de rendez-vous à destination des pharmacies.",
     technos: ["JAVASCRIPT", "REACT", "STORYBOOK"],
-    tags: ["front", "architecture", "specification", "design"],
+    tags: ["front", "design", "architecture", "specifications"],
     softs: ["GESTION", "TEAM", "LEADER", "COMMUNICATION", "MENTORING"],
     values: ["Refonte complète", "Gestion du legacy", "Respect des délais"],
     objective:
-      "L'objectif de ce projet était la refonte front complète d'un back office comprenant plusieurs applications complexes à destination de centaines de pharmacies adhérentes. Sur une durée de deux ans l'objectif était de développer de nouveaux composants et de les faire co-exister avec les anciens sans que cela pertube l'expérience utilisateur des pharmaciens.",
+      "L'objectif de ce projet était la refonte front complète d'un back office comprenant plusieurs applications complexes à destination de centaines de pharmacies adhérentes. Sur une durée de deux ans l'objectif était de développer de nouveaux composants et de les faire co-exister avec les anciens sans que cela ne pertube l'expérience utilisateur des pharmaciens.",
     technology:
-      "Le projet a été réalisé et maintenu avec React. La librairie de composant a été gérée et maintenue depuis un Storybook. Au terme de la refonte, l'application tournait sous Node 20 et React 18, garantissant ainsi sa mise à niveau technique et sa future maintenance.",
+      "Le projet a été réalisé et maintenu avec React. La librairie de composant a été réalisée et maintenue depuis un Storybook. Au terme de la refonte, l'application tournait sous Node 20 et React 18, garantissant ainsi sa mise à niveau technique et sa future maintenance.",
     role: "Mon rôle dans ce projet était la gestion complète de l'équipe de conception et de développement. Validation des spécifications fonctionnelles, des spécifications techniques et des maquettes. Conception, réalisation et maintenance technique de toutes les applications front du projet.",
-  },
-  {
-    id: "4bdb1d15-c689-4905-ba8e-4d9cbad7fdf0",
-    images: [
-      "/images/screenshots/VALWIN_PHARMA_1.webp",
-      "/images/screenshots/VALWIN_PHARMA_2.webp",
-      "/images/screenshots/VALWIN_PHARMA_3.webp",
-    ],
-    link: "https://www.pharmabest.com/",
-    title: "Implémentation Click and Collect",
-    description:
-      "Implémentation e-commerce sur site existant. Gestion dynamique du catalogue depuis un backoffice vers Algolia.",
-    technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
-    tags: ["front", "back", "design", "architecture"],
-    softs: ["GESTION", "TEAM", "LEADER", "COMMUNICATION", "MENTORING"],
-    values: ["gestion du legacy", "gestion du projet", "respet des délais"],
-    objective:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    technology:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    role: "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-  },
-  {
-    id: "6c46cccb-6159-4668-992c-6d622d634e90",
-    images: [
-      "/images/screenshots/APTIPHAR_1.webp",
-      "/images/screenshots/APTIPHAR_2.webp",
-      "/images/screenshots/APTIPHAR_3.webp",
-    ],
-    link: "https://www.pharmabest.com/",
-    title: "Site de présentation avec carte dynamique",
-    description:
-      "Implémentation e-commerce sur site existant. Gestion dynamique du catalogue depuis un backoffice vers Algolia.",
-    technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
-    tags: ["front", "back", "design", "architecture"],
-    softs: ["GESTION", "TEAM", "LEADER", "COMMUNICATION", "MENTORING"],
-    values: ["gestion du legacy", "gestion du projet", "respet des délais"],
-    objective:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    technology:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    role: "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-  },
-  {
-    id: "64340727-6b70-4131-8a5e-d54153e2a3c9",
-    images: [
-      "/images/screenshots/CORPO_1.webp",
-      "/images/screenshots/CORPO_2.webp",
-      "/images/screenshots/CORPO_3.webp",
-    ],
-    link: "https://www.pharmabest.com/",
-    title: "Site de présentation corporate",
-    description:
-      "Implémentation e-commerce sur site existant. Gestion dynamique du catalogue depuis un backoffice vers Algolia.",
-    technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
-    tags: ["front", "back", "design", "architecture"],
-    softs: ["GESTION", "TEAM", "LEADER", "COMMUNICATION", "MENTORING"],
-    values: ["gestion du legacy", "gestion du projet", "respet des délais"],
-    objective:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    technology:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    role: "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-  },
-  {
-    id: "fa2ed302-57ca-48c8-afc9-46d13efa1dac",
-    images: [
-      "/images/screenshots/COS_1.webp",
-      "/images/screenshots/COS_2.webp",
-      "/images/screenshots/COS_3.webp",
-    ],
-    link: "https://www.pharmabest.com/",
-    title: "Site de présentation avec carte dynamique",
-    description:
-      "Implémentation e-commerce sur site existant. Gestion dynamique du catalogue depuis un backoffice vers Algolia.",
-    technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
-    tags: ["front", "back", "design", "architecture"],
-    softs: ["GESTION", "TEAM", "LEADER", "COMMUNICATION", "MENTORING"],
-    values: ["gestion du legacy", "gestion du projet", "respet des délais"],
-    objective:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    technology:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    role: "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
   },
   {
     id: "47c6c2ab-9adc-45ee-b584-62eefe8deebf",
@@ -111,19 +27,119 @@ const projects = [
       "/images/screenshots/PHARMABEST_2.webp",
       "/images/screenshots/PHARMABEST_3.webp",
     ],
-    link: "https://www.pharmabest.com/",
-    title: "Implémentation Click and Collect",
+    link: "https://grande-pharmacie-paris-nantes.pharmabest.com",
+    title: "Implémentation livraison avec Colissimo",
+    star: true,
     description:
-      "Implémentation e-commerce sur site existant. Gestion dynamique du catalogue depuis un backoffice vers Algolia.",
-    technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
-    tags: ["front", "back", "design", "architecture"],
+      "Sur un site e-commerce, implémentation d'un module de livraison par Colissimo, le tout piloté depuis un back office.",
+    technos: ["JAVASCRIPT", "REACT", "NODE"],
+    tags: ["front", "design", "architecture", "specifications"],
     softs: ["GESTION", "TEAM", "LEADER", "COMMUNICATION", "MENTORING"],
-    values: ["gestion du legacy", "gestion du projet", "respet des délais"],
+    values: [
+      "Gestion du legacy",
+      "Implémentation livraison",
+      "Respect des délais",
+    ],
     objective:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
+      "L'objectif du projet était l'implémentation de la livraison à domicile et en point relais sur un site de e-commerce. Plusieurs options de configuration de livraison devait être présentes depuis un back office",
     technology:
-      "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
-    role: "Notre projet web tech vise à créer une plateforme innovante qui révolutionnera l'expérience en ligne. En combinant des technologies de pointe avec une conception intuitive, nous aspirons à offrir aux utilisateurs une expérience fluide et immersive.",
+      "Le module à destination du client a été réalisé sur un site e-commerce déjà existant créé avec React. Le module à destination du vendeur a été réalisé sur un back office déjà existant créé avec React.",
+    role: "Mon rôle dans ce projet était la gestion complète de l'équipe de conception et de développement. Validation des spécifications fonctionnelles, des spécifications techniques et des maquettes. Conception, réalisation et maintenance technique de toute l'implémentation front du projet.",
+  },
+  {
+    id: "4bdb1d15-c689-4905-ba8e-4d9cbad7fdf0",
+    images: [
+      "/images/screenshots/VALWIN_PHARMA_1.webp",
+      "/images/screenshots/VALWIN_PHARMA_2.webp",
+      "/images/screenshots/VALWIN_PHARMA_3.webp",
+    ],
+    star: true,
+    link: "https://pharma-solea.fr/",
+    title: "Refonte graphique et technique pour site de pharmacie",
+    description:
+      "Refonte graphique et technique de sites dynamiques de pharmacie proposant des applications de messagerie, d'e-commerce et de rendez-vous.",
+    technos: ["JAVASCRIPT", "REACT", "NODE", "STORYBOOK"],
+    tags: ["front", "design", "architecture", "specifications"],
+    softs: ["GESTION", "TEAM", "LEADER", "COMMUNICATION", "MENTORING"],
+    values: ["Gestion du legacy", "Gestion du projet", "Respect des délais"],
+    objective:
+      "L'objectif de ce projet était la refonte graphique et technique des pages de présentation de sites de pharmacies. Le contenu et les couleurs des sections devaient être dynamiquement gérés depuis un back office.",
+    technology:
+      "Le projet a été réalisé et maintenu avec React. La librairie de composant a été réalisée et maintenue depuis un Storybook.",
+    role: "Mon rôle dans ce projet était la gestion complète de l'équipe de conception et de développement. Validation des spécifications fonctionnelles, des spécifications techniques et des maquettes. Conception, réalisation et maintenance technique de toutes les sections des pages de présentation.",
+  },
+  {
+    id: "fa2ed302-57ca-48c8-afc9-46d13efa1dac",
+    images: [
+      "/images/screenshots/COS_1.webp",
+      "/images/screenshots/COS_2.webp",
+      "/images/screenshots/COS_3.webp",
+    ],
+    link: "https://pharmaciecos.fr/",
+    title: "Site de présentation de groupement de pharmacies",
+    description:
+      "Réalisation d'un site de présentation avec carte dynamique et formulaire de contact.",
+    technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
+    tags: ["front", "architecture"],
+    softs: ["TEAM", "MENTORING"],
+    values: [
+      "Création de site",
+      "Travail avec le client",
+      "Respect des délais",
+    ],
+    objective:
+      "Réaliser en collaboration avec le client, l'objectif était la réalisation d'un site de présentation avec une carte dynamique éditable depuis un back office et d'un formulaire de contact simple.",
+    technology:
+      "Le projet a été réalisé et maintenu avec React. La carte dynamique a été réalisée avec Google Maps.",
+    role: "Conception, réalisation et maintenance technique du site sous la direction d'un directeur technique et d'une project manager.",
+  },
+  {
+    id: "64340727-6b70-4131-8a5e-d54153e2a3c9",
+    images: [
+      "/images/screenshots/CORPO_1.webp",
+      "/images/screenshots/CORPO_2.webp",
+      "/images/screenshots/CORPO_3.webp",
+    ],
+    link: "https://www.valwin.fr/",
+    title: "Création site de présentation pour une marque logicielle",
+    description:
+      "Pour une marque logicielle, réalisation d'un site vitrine avec gestion dynamique du contenu depuis un back office.",
+    technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
+    tags: ["front", "architecture"],
+    softs: ["TEAM", "MENTORING"],
+    values: [
+      "Création de site",
+      "Travail avec le client",
+      "Respect des délais",
+    ],
+    objective:
+      "Création de site vitrine pour présentation de l'entreprise avec une gestion de contenu depuis un back office.",
+    technology: "Le projet a été réalisé et maintenu avec React.",
+    role: "Conception, réalisation et maintenance technique du site sous la direction d'un directeur technique et d'une project manager.",
+  },
+  {
+    id: "6c46cccb-6159-4668-992c-6d622d634e90",
+    images: [
+      "/images/screenshots/APTIPHAR_1.webp",
+      "/images/screenshots/APTIPHAR_2.webp",
+      "/images/screenshots/APTIPHAR_3.webp",
+    ],
+    title: "Site de présentation de groupement de pharmacies",
+    description:
+      "Réalisation d'un site de présentation avec carte dynamique et formulaire de contact.",
+    technos: ["HTML", "CSS", "JAVASCRIPT", "NODE"],
+    tags: ["front", "architecture"],
+    softs: ["TEAM", "MENTORING"],
+    values: [
+      "Création de site",
+      "Travail avec le client",
+      "Respect des délais",
+    ],
+    objective:
+      "Réaliser en collaboration avec le client, l'objectif était la réalisation d'un site de présentation avec une carte dynamique éditable depuis un back office et d'un formulaire de contact simple.",
+    technology:
+      "Le projet a été réalisé et maintenu avec React. La carte dynamique a été réalisée avec Google Maps.",
+    role: "Conception, réalisation et maintenance technique du site sous la direction d'un directeur technique et d'une project manager.",
   },
 ];
 
