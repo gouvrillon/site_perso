262d4b3f0958f5b6a91af1cbb110964f30974e2b
Thomas Gouvrillon <thomasgouvrillon@MacBook-Air-de-Thomas.local>
2024-02-08 01:34:31 +0100

    feat : style modal

--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { node, func, bool, string } from "prop-types";
 import "./Modal.css";
 import classnames from "classnames";
 import Icon from "../Icon/Icon.jsx";
 
 const Modal = ({ darkMode, setModal, children, title }) => {
+  const [displayed, setDisplayed] = useState(false);
+
+  useEffect(() => {
+    setDisplayed(true);
+  }, [children]);
+
   return (
-    <div className="Modal">
-      <div className="Modal__background" onClick={() => setModal(null)}></div>
+    <div
+      className={classnames("Modal", {
+        "Modal--displayed": displayed,
+      })}
+    >
+      <div
+        className={classnames("Modal__background", {
+          "Modal__background--darkMode": darkMode,
+        })}
+        onClick={() => setModal(null)}
+      ></div>
       <div
         className={classnames("Modal__content", {
           "Modal__content--darkMode": darkMode,
         })}
       >
         <div className="Modal__content__header">
-          <div className="Modal__content__title">{title}</div>
-          <div className="Modal__content__icon" onClick={() => setModal(null)}>
+          <div
+            className={classnames("Modal__content__title", {
+              "Modal__content__title--darkMode": darkMode,
+            })}
+          >
+            {title}
+          </div>
+          <div
+            className={classnames("Modal__content__icon", {
+              "Modal__content__icon--darkMode": darkMode,
+            })}
+            onClick={() => setModal(null)}
+          >
             <Icon icon="CROSS" />
           </div>
         </div>
