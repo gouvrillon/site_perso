9a346c1f0d5238701e457b2be5420f5a08ead5e4
Thomas Gouvrillon <thomasgouvrillon@MacBook-Air-de-Thomas.local>
2024-02-12 18:01:25 +0100

    feat : remove href on projects if empty

--- components/ModalProject/ModalProject.jsx
+++ components/ModalProject/ModalProject.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import "./ModalProject.css";
 import { string, bool } from "prop-types";
 import Linky from "../Linky/Linky.jsx";
+import isEmpty from "lodash/isEmpty";
 
 const ModalProject = ({ src, href, darkMode }) => {
   return (
     <div className="ModalProject">
       <img src={src} className="ModalProject__image" />
-      <Linky
-        label="Voir le site"
-        href={href}
-        darkMode={darkMode}
-        isExternal={true}
-      />
+      {!isEmpty(href) && (
+        <Linky
+          label="Voir le site"
+          href={href}
+          darkMode={darkMode}
+          isExternal={true}
+        />
+      )}
     </div>
   );
 };
